e3a51224e8596c6fb40b5a9b09b2af8e02a6d712
grangegermi <dasha.voloshina.9883@gmail.com>
2025-05-17 16:00:30 +0300

    added Project

---
+++ News/News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Даша Волошина on 17.05.25.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
